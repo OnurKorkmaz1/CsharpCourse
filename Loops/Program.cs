586b7520288c8befa8132d6d43c2917b563961fd
OnurKorkmaz <korkmazo1903@gmail.com>
2024-07-01 21:54:42 +0300

    added some example

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -8,8 +8,41 @@ namespace Loops
 {
     internal class Program
     {
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number-1; i++)
+            {
+                if (number%i == 0)
+                {
+                    result = false;
+                    i = number;
+
+                }        
+
+            }
+            return result;
+
+        }
+
+
+
+
+
         static void Main(string[] args)
         {
+            if (IsPrimeNumber(6)) 
+            {
+                Console.WriteLine("this is a prime number");
+            }
+            else
+            {
+                Console.WriteLine("this is not a prime number");
+            }
+
+
+
+
             for (int i = 0; i < 100; i++) // -- burada tanımlanan i = 0 sadece bu blok içerisinde geçerlidir.
             {
                 Console.WriteLine(i);
