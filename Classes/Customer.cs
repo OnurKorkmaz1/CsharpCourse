935664ddf7bca86f34e32dbdb20ab1ecbf6cae0e
OnurKorkmaz <korkmazo1903@gmail.com>
2024-07-01 22:41:01 +0300

    class&methods

---
+++ Classes/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Customer
+    {
+        public int Id { get; set; }   // müşterilerin verilerini tutmak için kullanabileceğimiz kısım.
+        public string FirstName { get; set; }  // buna property tanımlamak deniyor.
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            Customer customer = new Customer();
+
+            customer.Address = "Ankara";
+            customer.FirstName = "Onur";
+            customer.LastName = "Korkmaz";
+            customer.Id = 1;
+
+            Customer customer2 = new Customer();
+            {
+                
+            }
+
+            Console.WriteLine(customer2.Address);
+            Console.WriteLine();
+        }
+    }
+    class CustomerManager                                        // Class
+    {
+        public void Add()                                        // Method
+        {
+            Console.WriteLine("Customer Added");
+        }
+        public void Update()                                    // Method
+        {
+            Console.WriteLine("Customer Updated");      
+        }
+        
+    }
+}
